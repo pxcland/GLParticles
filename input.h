dfb3bd56518cb4910a985ada5d1ff6d427c4b04d
Patrick Cland <pxcland@gmail.com>
2017-01-14 06:02:25 -0600

    Update input.h

--- input.h
+++ input.h
@@ -8,14 +8,14 @@
 
 extern Uint8* key;
 
-#define EMITTER_VEL			0.0001f
-#define EMITTER_ANGV		0.000005f
-#define EMITTER_VELV		0.000005f
-#define EMITTER_GRAVV		0.000001f
-#define EMITTER_COLV		0.000001f
-#define EMITTER_SIZEV		0.000005f
-#define EMITTER_DEATHRATEV	0.000001f
-#define	EMITTER_EMITTINGV	0.00002f
+#define EMITTER_VEL         0.0001f
+#define EMITTER_ANGV        0.000005f
+#define EMITTER_VELV        0.000005f
+#define EMITTER_GRAVV       0.000001f
+#define EMITTER_COLV        0.000001f
+#define EMITTER_SIZEV       0.000005f
+#define EMITTER_DEATHRATEV  0.000001f
+#define	EMITTER_EMITTINGV   0.00002f
 
 void processInput(Uint8* key, Emitter* e);
 
